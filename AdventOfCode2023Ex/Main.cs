c2807521b8e31731047473ff87ec2b6002a8eef2
Evan Kochuk <35906327+RavenDreamer@users.noreply.github.com>
2023-12-07 23:02:38 -0500

    Advent of Code Day 7

--- AdventOfCode2023Ex/Main.cs
+++ AdventOfCode2023Ex/Main.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine(Puzzle_6_2.Execute());
+Console.WriteLine(Puzzle_7.Puzzle_7_2.Execute());
